b9d711661be41df325a73afd5a4be3d9eceed9b4
Pchristman <76259823+Pchristman@users.noreply.github.com>
2021-08-30 21:19:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ import random
 from hangman_words import word_list
 from hangman_art import logo, stages
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-print(logo)
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -25,7 +20,6 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
       print(f"You've already guess the lettter: {guess}")
 
@@ -38,7 +32,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"The letter: {guess} is not in the word")
         lives -= 1
         if lives == 0:
@@ -53,5 +46,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
